ac19891960768c225a148e1ec109f162b85853ba
HaBV4 <35767439+HaBV4@users.noreply.github.com>
2020-07-21 15:09:17 +0700

    Delete Favorites

--- src/components/Favorite.js
+++ src/components/Favorite.js
@@ -1,9 +1,11 @@
 import React, {useState} from 'react';
-import {View, StyleSheet, FlatList, Text} from 'react-native';
+import {View, StyleSheet, FlatList, Text, ScrollView} from 'react-native';
 import {ListItem, Tile} from 'react-native-elements';
 import {connect} from 'react-redux';
 import {baseURL} from '../shared/baseURL';
 import Loading from './Loading';
+import {deleteFavorite} from '../redux/ActionCreators';
+import Swipeout from 'react-native-swipeout';
 
 const mapStateToProps = (state) => {
   return {
@@ -12,18 +14,32 @@ const mapStateToProps = (state) => {
   };
 };
 
+const mapDispatchToProps = (dispatch) => ({
+  deleteFavorite: (dishId) => dispatch(deleteFavorite(dishId)),
+});
+
 const Favorite = (props) => {
   const {navigate} = props.navigation;
+
   const RenderMenuItem = ({item, index}) => {
+    const RightButton = [
+      {
+        text: 'Delete',
+        type: 'delete',
+        onPress: () => props.deleteFavorite(item.id),
+      },
+    ];
     return (
-      <ListItem
-        key={index}
-        title={item.name}
-        subtitle={item.description}
-        chevron={false}
-        onPress={() => navigate('DishDetail', {dishId: item.id})}
-        leftAvatar={{source: {uri: baseURL + item.image}}}
-      />
+      <Swipeout right={RightButton} autoClose={true}>
+        <ListItem
+          key={index}
+          title={item.name}
+          subtitle={item.description}
+          chevron={false}
+          onPress={() => navigate('DishDetail', {dishId: item.id})}
+          leftAvatar={{source: {uri: baseURL + item.image}}}
+        />
+      </Swipeout>
     );
   };
 
@@ -49,4 +65,4 @@ const Favorite = (props) => {
 };
 const styles = StyleSheet.create({});
 
-export default connect(mapStateToProps)(Favorite);
+export default connect(mapStateToProps, mapDispatchToProps)(Favorite);
